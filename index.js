5b4cc6d7c9aa4c91e21a03b364ce803d3203d2f1
digital-programmer <mohandibya123@gmail.com>
2022-01-14 23:07:37 +0530

    update port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 const cors = require('cors');
-const port = 8000;
+const port = process.env.port || 8000;
 const db = require("./config/mongoose");
 
 
