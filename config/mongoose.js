da1a08da2169b65f12a2239efef77de5af253d7f
digital-programmer <mohandibya123@gmail.com>
2022-01-14 22:41:46 +0530

    set env variables

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/ecommerce-rest-cn");
+mongoose.connect(`${process.env.DB_URL}`);
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "Error conecting to MongoDB"));
